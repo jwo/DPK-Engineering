f5032d9c98050c429d0f1a4f627874ad789cc8a4
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2009-10-16 21:59:41 -0500

    brought images and style from designer'

--- app.rb
+++ app.rb
@@ -1,3 +1,31 @@
 require 'rubygems'
 require 'sinatra'
 
+get '/' do
+ erb :index
+end
+
+#rather than have 8 different methods like:
+#=====
+#get '/resume' do
+#  erb :resume
+#end
+#=====
+#I created a method to load if page found
+
+get '/:call' do
+  begin
+  erb params[:call].to_sym
+  
+  #if the file does not exist
+  rescue Errno::ENOENT => e
+   pass
+  end
+end
+
+
+helpers do
+  def nav_active(route, current_path)
+    lib_nav_active(route, current_path)
+   end
+end
