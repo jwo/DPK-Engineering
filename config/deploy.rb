6ed5d7ecd8562f3036e899fd045c03c632a2b6d2
Jesse Wolgamott <jesse.wolgamott@gmail.com>
2009-10-20 14:20:34 -0500

    Deploy commits

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, 'dpk-engineering'
+#
+# GitHub settings #######################################################################################
+#
+default_run_options[:pty] = true
+set :repository, "git@github.com:jwo/DPK-Engineering.git" #GitHub clone URL
+set :scm, "git"
+#set :scm_passphrase, "notused" #This is the passphrase for the ssh key on the server deployed to
+set :branch, "master"
+set :scm_verbose, true
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
-
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "dpk.jessewolgamott.com"
+role :web, "dpk.jessewolgamott.com"
+role :db,  "dpk.jessewolgamott.com", :primary => true
